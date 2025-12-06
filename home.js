53e9d151a1924ccd2b80e1b0ea746a137b76ce5b
Parshotam <parshotampragani52@gmail.com>
2025-12-06 13:01:41 +0500

    Updated

---
+++ home.js
@@ -0,0 +1,6 @@
+let input = document.getElementById("input");
+let addBtn = document.getElementById("add");
+
+input.addEventListener("click", function () {
+    let add = document.getElementById("add");
+})
